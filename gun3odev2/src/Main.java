677827c877aa6a8bda259e5fd621f5646e11212f
furkansefademirci <52625217+furkansefademirci@users.noreply.github.com>
2021-05-01 18:33:30 +0300

    Update Main.java

--- gun3odev2/src/Main.java
+++ gun3odev2/src/Main.java
@@ -3,15 +3,14 @@ public class Main {
 
         Student student1 = new Student("Furkan", "email@gmail.com", "123");
         Student student2 = new Student("Sefa", "email@hotmail.com", "456");
-        User user = new User("as", "sd", "sd");
+        User user = new User("user", "user@email.com", "456");
         Instructer instructer = new Instructer("FSD", "fsd@fsd.com", "789");
 
         UserManager userManager = new UserManager();
         InstructerManager instructerManager = new InstructerManager();
 
         Student[] students = {student1, student2};
-        user.setName("sad");
-
+       
         userManager.changePhoneNumber(student1, "321");
 
         for (Student student : students) {
